d72ddb8688709f25540889b74256d8b6e7e1f278
Cookiez152 <49290434+Cookiez152@users.noreply.github.com>
2021-01-17 03:45:07 +0000

    Add files via upload

---
+++ DNSReset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Running will reset and restart DNS service on Parrot OS due to issues with AnonSurf.
+#Source:  https://askubuntu.com/questions/1106659/how-to-reset-dns-settings-to-the-defaults  
+#Austin Pasquel
+
+sudo rm /etc/resolv.conf && sudo ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+
+systemctl status systemd-resolved.service | if ! grep -q 'active (running)'; then 
+    echo "Restarting DNS Service"
+    systemctl enable systemd-resolved.service && systemctl start systemd-resolved.service
+else
+    echo "DNS Service Enabled"
+fi
